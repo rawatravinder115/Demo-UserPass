eb0f212aaf6883b8d290c204ad545728deeedb39
rawatravinder115 <rawatravinder115@gmail.com>
2021-06-29 10:24:03 +0530

    add locale browser storage

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem("IsLoggedIn"); /* to get the element from locale storage of the browser */
+
+    if (storedUserLoggedInInformation === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);  /* useEffect has a anonymous func and array[] of dependency */
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn',1); /* storing key value pair in local storage in browser so track of logged in or not  */ 
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('IsLoggedIn');
     setIsLoggedIn(false);
   };
 
